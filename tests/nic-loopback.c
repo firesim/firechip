8604f86cc1eb0b09b68f2732b8ba7c14041e5b72
Howard Mao <zhehao.mao@gmail.com>
2017-10-22 21:28:36 +0000

    more rigorous loopback test with timing

--- tests/nic-loopback.c
+++ tests/nic-loopback.c
@@ -1,6 +1,7 @@
 #include "mmio.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "nic.h"
 
@@ -8,29 +9,57 @@ uint64_t src[200];
 uint64_t dst[200];
 
 #define TEST_LEN 128
+#define NTRIALS 3
 
-int main(void)
+static unsigned long rdcycle(void)
 {
-	int i, len;
+	unsigned long cycles;
 
-	for (i = 0; i < TEST_LEN; i++)
-		src[i] = i << 12;
+	asm volatile ("rdcycle %[cycles]" : [cycles] "=r" (cycles));
+	return cycles;
+}
 
-	printf("Sending data on loopback NIC\n");
+void run_test(void)
+{
+	unsigned long start, end, len;
+	int i;
 
+	memset(dst, 0, sizeof(uint64_t) * TEST_LEN);
 	asm volatile ("fence");
-	nic_send(src, TEST_LEN * sizeof(uint64_t));
-	len = nic_recv(dst);
 
-	printf("Received %d bytes\n", len);
+	start = rdcycle();
+	nic_send(src, TEST_LEN * sizeof(uint64_t));
+	end = rdcycle();
+	printf("send %lu cycles\n", end - start);
+
+	start = rdcycle();
+	len = nic_recv(dst);
+	end = rdcycle();
+	printf("recv %lu cycles\n", end - start);
+
+	if (len != TEST_LEN * sizeof(uint64_t)) {
+		printf("recv got wrong # bytes\n");
+		exit(EXIT_FAILURE);
+	}
 
 	for (i = 0; i < TEST_LEN; i++) {
 		if (dst[i] != src[i]) {
 			printf("Data mismatch @ %d: %lx != %lx\n",
 					i, dst[i], src[i]);
-			return 1;
+			exit(EXIT_FAILURE);
 		}
 	}
+}
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < TEST_LEN; i++)
+		src[i] = i << 12;
+
+	for (i = 0; i < NTRIALS; i++)
+		run_test();
 
 	printf("All correct\n");
 
