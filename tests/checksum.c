d8f19e809c3d97d7852598163c03b800cfd38c17
Howard Mao <zhehao.mao@gmail.com>
2018-05-21 21:45:04 -0700

    implement multi-segment checksum offloading in IceNIC

--- tests/checksum.c
+++ tests/checksum.c
@@ -5,15 +5,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define DATA_SIZE 64
-#define DATA_START 1
-
-uint16_t calc_checksum(uint16_t *data, int n)
+uint16_t calc_checksum(void **data, int *sizes, int n)
 {
 	uint32_t sum = 0;
 
-	for (int i = 0; i < n; i++)
-		sum += data[i];
+	for (int i = 0; i < n; i++) {
+		int m = sizes[i] / sizeof(uint16_t);
+		uint16_t *words = (uint16_t *) data[i];
+		for (int j = 0; j < m; j++)
+			sum += words[j];
+	}
 
 	while ((sum >> 16) != 0)
 		sum = (sum & 0xffff) + (sum >> 16);
@@ -21,19 +22,22 @@ uint16_t calc_checksum(uint16_t *data, int n)
 	return ~sum & 0xffff;
 }
 
-uint16_t request_checksum(void *data, int size)
+uint16_t request_checksum(void **data, int *sizes, int n)
 {
-	uintptr_t request = (uintptr_t) data;
-	uintptr_t length = (uintptr_t) size;
+	uintptr_t request, length, partial;
 	int avail;
-
-	request |= (length << 48);
 
 	do {
 		avail = reg_read16(SIMPLENIC_CKSUM_COUNTS) & 0xff;
-	} while (avail <= 0);
+	} while (avail < n);
 
-	reg_write64(SIMPLENIC_CKSUM_REQ, request);
+	for (int i = 0; i < n; i++) {
+		request = (uintptr_t) data[i];
+		length = sizes[i];
+		partial = i < (n - 1);
+		request |= (partial << 63) | (length << 48);
+		reg_write64(SIMPLENIC_CKSUM_REQ, request);
+	}
 
 	do {
 		avail = (reg_read16(SIMPLENIC_CKSUM_COUNTS) >> 8) & 0xff;
@@ -42,23 +46,33 @@ uint16_t request_checksum(void *data, int size)
 	return reg_read16(SIMPLENIC_CKSUM_RESP);
 }
 
+#define NSEGS 4
+#define NDATA 128
+
 int main(void)
 {
-	uint16_t data[DATA_SIZE];
+	uint16_t data[NDATA];
+	int i, idx = 0;
+	int nwords[NSEGS] = {33, 4, 64, 27};
+	int sizes[NSEGS];
+	void *ptrs[NSEGS];
 	uint16_t expected, actual;
-	int offset = 1;
-	int len = DATA_SIZE-2;
-	int nbytes = len * sizeof(uint16_t);
 	int cid = 0;
 
-	for (int i = 0; i < DATA_SIZE; i++)
+	for (i = 0; i < NDATA; i++)
 		data[i] = i;
 
+	for (i = 0; i < NSEGS; i++) {
+		ptrs[i] = &data[idx];
+		sizes[i] = nwords[i] * sizeof(uint16_t);
+		idx += nwords[i];
+	}
+
 	printf("Calculating expected checksum");
-	stats(expected = calc_checksum(&data[offset], len), len);
+	stats(expected = calc_checksum(ptrs, sizes, NSEGS), NDATA);
 
 	printf("Getting checksum from accelerator");
-	stats(actual = request_checksum(&data[offset], nbytes), len);
+	stats(actual = request_checksum(ptrs, sizes, NSEGS), NDATA);
 
 	if (expected != actual) {
 		printf("Incorrect result expected: %04x, actual: %04x\n",
