837262b7512623d30ebd70744c37e4d85ced7be5
Howard Mao <zhehao.mao@gmail.com>
2018-12-24 20:32:18 -0800

    clean up imports in dramcachetg Top.scala

--- src/main/scala/dramcachetg/Top.scala
+++ src/main/scala/dramcachetg/Top.scala
@@ -2,11 +2,7 @@ package dramcachetg
 
 import chisel3._
 import freechips.rocketchip.config.Parameters
-import freechips.rocketchip.groundtest._
-import freechips.rocketchip.diplomacy._
-import freechips.rocketchip.interrupts._
-import freechips.rocketchip.tilelink._
-import freechips.rocketchip.tile._
+import freechips.rocketchip.groundtest.{GroundTestSubsystem, GroundTestSubsystemModuleImp}
 import memblade.cache.{HasPeripheryDRAMCache, HasPeripheryDRAMCacheModuleImp}
 
 class DRAMCacheGroundTestTop(implicit p: Parameters) extends GroundTestSubsystem
