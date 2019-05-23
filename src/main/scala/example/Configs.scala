8caf3a9521044693780eaf49f8a516618df5885d
Howard Mao <zhehao.mao@gmail.com>
2019-05-22 18:45:24 -0700

    bump DRAM cache for multi-tracker banks

--- src/main/scala/example/Configs.scala
+++ src/main/scala/example/Configs.scala
@@ -83,7 +83,7 @@ class WithTestMemBlade extends Config((site, here, up) => {
   case NICKey => NICConfig(inBufFlits = 8640, usePauser = true)
   case BuildTop => (clock: Clock, reset: Bool, p: Parameters) => {
     val top = Module(LazyModule(new ExampleTopWithRemoteMemClient()(p)).module)
-    top.connectTestMemBlade()
+    top.connectTestMemBlade(latency = 100)
     top
   }
 })
@@ -98,15 +98,23 @@ class WithDRAMCache extends Config((site, here, up) => {
   case MemBenchKey => MemBenchParams(nXacts = 256)
   case NICKey => NICConfig(inBufFlits = 8640, usePauser = true)
   case MemBladeKey => MemBladeParams(
+    spanBytes = 1024,
     spanQueue = MemBladeQueueParams(reqHeadDepth = 32))
   case DRAMCacheKey => DRAMCacheConfig(
+    nTrackersPerBank = 2,
+    nBanksPerChannel = 2,
     nSets = 256,
     nWays = 7,
     baseAddr = 1L << 32,
+    spanBytes = 1024,
     extentBytes = 1 << 20,
     logAddrBits = 28,
     nSecondaryRequests = 15,
-    zeroMetadata = true)
+    nMetaCacheRows = 32,
+    zeroMetadata = true,
+    remAccessQueue = RemoteAccessDepths(1, 2, 1, 2),
+    wbQueue = WritebackDepths(1, 1),
+    memInQueue = MemoryQueueParams(2, 2))
   case BuildTop => (clock: Clock, reset: Bool, p: Parameters) => {
     val top = Module(LazyModule(new ExampleTopWithDRAMCache()(p)).module)
     top.connectTestMemBlade()
@@ -159,13 +167,17 @@ class RV32ExampleConfig extends Config(
   new WithRV32 ++ new DefaultExampleConfig)
 
 class DRAMCacheConfig extends Config(
-  new WithL2Size(64) ++
+  new WithNTrackersPerBank(7) ++
   new WithNBanksPerMemChannel(8) ++
-  new WithL2Latency(121) ++
-  new WithFederationL2Cache ++
   new WithPrefetchRoCC ++
   new WithDRAMCache ++
   new DefaultExampleConfig)
+
+class DRAMCacheL2Config extends Config(
+  new WithL2Size(16) ++
+  new WithL2Latency(121) ++
+  new WithFederationL2Cache ++
+  new DRAMCacheConfig)
 
 class PrefetcherConfig extends Config(
   new WithPrefetchRoCC ++ new DefaultExampleConfig)
