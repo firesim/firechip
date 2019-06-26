f05b48aa69dd2170565a80ef334414a2575560d6
Howard Mao <zhehao.mao@gmail.com>
2019-06-25 23:53:05 -0700

    use HasHierarchicalBusTopology trait

--- src/main/scala/example/Top.scala
+++ src/main/scala/example/Top.scala
@@ -13,6 +13,7 @@ import memblade.client.{HasPeripheryRemoteMemClient, HasPeripheryRemoteMemClient
 import memblade.cache.{HasPeripheryDRAMCache, HasPeripheryDRAMCacheModuleImp}
 
 class ExampleTop(implicit p: Parameters) extends RocketSubsystem
+    with HasHierarchicalBusTopology
     with CanHaveMasterAXI4MemPort
     with HasPeripheryBootROM
 //    with HasSystemErrorSlave
@@ -20,25 +21,6 @@ class ExampleTop(implicit p: Parameters) extends RocketSubsystem
     with HasNoDebug
     with HasPeripherySerial {
   override lazy val module = new ExampleTopModuleImp(this)
-
-  // The sbus masters the cbus; here we convert TL-UH -> TL-UL
-  sbus.crossToBus(cbus, NoCrossing)
-
-  // The cbus masters the pbus; which might be clocked slower
-  cbus.crossToBus(pbus, SynchronousCrossing())
-
-  // The fbus masters the sbus; both are TL-UH or TL-C
-  FlipRendering { implicit p =>
-    sbus.crossFromBus(fbus, SynchronousCrossing())
-  }
-
-  // The sbus masters the mbus; here we convert TL-C -> TL-UH
-  private val BankedL2Params(nBanks, coherenceManager) = p(BankedL2Key)
-  private val (in, out, halt) = coherenceManager(this)
-  if (nBanks != 0) {
-    sbus.coupleTo("coherence_manager") { in :*= _ }
-    mbus.coupleFrom("coherence_manager") { _ :=* BankBinder(mbus.blockBytes * (nBanks-1)) :*= out }
-  }
 }
 
 class ExampleTopModuleImp[+L <: ExampleTop](l: L) extends RocketSubsystemModuleImp(l)
